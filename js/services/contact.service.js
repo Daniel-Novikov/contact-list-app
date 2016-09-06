8fdf54f7a30a9d6623e373b41249a85a484fe23d
Daniel Novikov <daniel.d.novikov@gmail.com>
2016-09-06 23:47:22 +1200

    json

--- js/services/contact.service.js
+++ js/services/contact.service.js
@@ -2,7 +2,7 @@ angular.module('app')
 
 .factory('Contacts', ['$http', function($http) {
 
-    var urlBase = '/json/users.json', // http://jsonplaceholder.typicode.com/users
+    var urlBase = 'http://jsonplaceholder.typicode.com/users', //'/json/users.json',
         Contacts = {};
 
     Contacts.get = function() {
@@ -17,7 +17,7 @@ angular.module('app')
 
 .factory('Heroes', ['$http', function($http) {
 
-    var urlBase = '/json/heroes.json',
+    var urlBase = 'https://api.myjson.com/bins/27n4w', //'/json/heroes.json',
         Heroes = {};
 
     Heroes.get = function() {
